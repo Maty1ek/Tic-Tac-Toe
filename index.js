99251c8d50dcdde148fa95891c3a448bb0b4b375
Maty1ek <galymzakangerov@icloud.com>
2024-09-05 17:51:53 +0500

    created a layout for the main game page and connected it with the script

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-let gameBoard = [
+const player1Score = document.getElementById('player1Score')
+const player2Score = document.getElementById('player2Score')
+const roundsCount = document.getElementById('roundsCount')
+const squaresArray = document.querySelectorAll('.game_board_square')
+const gameBoard = document.getElementById('gameBoard')
+const tieButton = document.getElementById('tieButton')
+const tieWindow = document.getElementById('tieWindow')
+
+const gameSquares = Array.from(squaresArray)
+const xMark = '<i class="fa-solid fa-xmark xMark gameMarks"></i>'
+const oMark = '<i class="fa-regular fa-circle oMark gameMarks"></i>'
+
+const virtualGameBoard = [
     '', '', '',
     '', '', '',
     '', '', ''
 ]
 
-let winningCombinations = [
+const winningCombinations = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -19,55 +31,79 @@ let isCurrentX = true
 let hasWon = false
 let theWinner = ''
 
+let player1Count = 0
+let player2Count = 0
+
+let gameRounds = 5
+let roundCount = 0
+
+tieButton.addEventListener('click', tieToggle)
+
 function setPlayerMark(mark, index) {
     if (index >= 0 && index <= 8) {
-        gameBoard[index] = mark
+        virtualGameBoard[index] = mark
     }
 }
 
-for (let i = 0; i < gameBoard.length; i++) {
-    if(!hasWon) {
-        let markPlace = Number(prompt('Enter the place of your mark (0-8)'))
-        if (isCurrentX && gameBoard[markPlace] === '') {
-            setPlayerMark('x', markPlace)
-            isCurrentX = false
-        } else if (!isCurrentX && gameBoard[markPlace] === '') {
-            setPlayerMark('o', markPlace)
-            isCurrentX = true
-        } else if (!markPlace) {
-            break
-        } else {
-            i--
+function tieToggle() {
+    gameBoard.classList.toggle('none')
+    tieWindow.classList.toggle('none')
+}
+
+for (let i = 0; i < gameSquares.length; i++) {
+    gameSquares[i].addEventListener('click', () => {
+        if (!hasWon) {
+            if (isCurrentX && virtualGameBoard[i] === '') {
+                setPlayerMark('x', i)
+                gameSquares[i].innerHTML = xMark
+                isCurrentX = false
+            } else if (!isCurrentX && virtualGameBoard[i] === '') {
+                setPlayerMark('o', i)
+                gameSquares[i].innerHTML = oMark
+                isCurrentX = true
+            }
+            if (!virtualGameBoard.includes('')) {
+                tieToggle()
+                clearGameBoard()
+            }
+            checkTheWInner(i)
         }
-        checkTheWInner2(markPlace)
-    } else if(hasWon) {
-        alert(`the winner is ${theWinner}`)
-        break
+
+    })
+}
+
+function clearGameBoard() {
+    for (let i = 0; i < virtualGameBoard.length; i++) {
+        virtualGameBoard[i] = ''
     }
+    for (let i = 0; i < gameSquares.length; i++) {
+        gameSquares[i].innerHTML = ''
+    }
+    hasWon = false
+    isCurrentX = true
+    roundCount++
+    theWinner = ''
+    // if(roundCount == gameRounds) {
+
+    // }
 }
 
-if(!hasWon) {
-    alert('tie')
-}
-
-
-function checkTheWInner2(index) {
+function checkTheWInner(index) {
     for (let i = 0; i < winningCombinations.length; i++) {
         if (winningCombinations[i].includes(index)) {
-            const [a,b,c] = winningCombinations[i]
-            console.log('yes',index, a, b, c);
-            if(gameBoard[a] == gameBoard[b] && gameBoard[b] == gameBoard[c]) {
+            const [a, b, c] = winningCombinations[i]
+            if (virtualGameBoard[a] == virtualGameBoard[b] && virtualGameBoard[b] == virtualGameBoard[c]) {
                 hasWon = true
-                theWinner = gameBoard[b]
-                console.log('yes222', gameBoard[b]);
+                theWinner = virtualGameBoard[c]
+                if (theWinner == 'x') {
+                    player1Count++
+                    player1Score.innerHTML = player1Count
+                } else if (theWinner == 'o') {
+                    player2Count++
+                    player2Score.innerHTML = player2Count
+                }
+                clearGameBoard()
             }
         }
-    }
-}
-
-function checkTheWinner(index) {
-    for (let i = 0; i < winningCombinations.length; i++) {
-        const [a, b, c] = winningCombinations[i]
-
     }
 }
